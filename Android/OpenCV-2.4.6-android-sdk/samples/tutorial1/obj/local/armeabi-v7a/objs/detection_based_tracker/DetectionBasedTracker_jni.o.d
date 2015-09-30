84d5dd144bb45b9c37dc24e52ba03afd9bb4892f
Fabio <fabiocasagrande86@gmail.com>
2015-09-30 16:05:34 -0400

    Android Software

---
+++ Android/OpenCV-2.4.6-android-sdk/samples/tutorial1/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/jni.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/assert.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdlib.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/string.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/malloc.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/alloca.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/strings.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdint.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/memory.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/math.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/limits.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/limits.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/limits.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/limits.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/jni.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/assert.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdlib.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/string.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/alloca.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/strings.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/memory.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/math.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/limits.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+C:/Users/Fabio/Documents/eclipse/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/android/log.h:
